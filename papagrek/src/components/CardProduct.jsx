7770d0093eb925e97ea0f1c5350d5dbcec6d8cf3
Alexey Zhumaev <alexey221284@mail.ru>
2021-04-14 12:56:52 +0400

    add new url v5 /img in db.json

--- papagrek/src/components/CardProduct.jsx
+++ papagrek/src/components/CardProduct.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import PropTypes from "prop-types";
 import "../App.css"
-//import srcPhoto from "../img/products/1/2.jpg";
 
 class CardProduct extends React.Component {
 	render() {
 		const { title, price, urlPhoto } = this.props;
-		let srcPhoto = urlPhoto;
-		console.log('urlPhoto ' + urlPhoto)
-		console.log('srcPhoto ' + srcPhoto)
+		console.log('urlPhoto ' + urlPhoto);
 
 		return(
 			<div className="cardProduct">
-				<img src={srcPhoto} alt="здесь должно быть фото товара"/>
+				<img src={urlPhoto} alt="здесь должно быть фото товара"/>
 				<h3>{title}</h3>
 				<h2>Цена {price} руб.</h2>
 			</div>
