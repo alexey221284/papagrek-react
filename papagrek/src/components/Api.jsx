481d6e49320190ac796268f6073227df516f4eae
Alexey Zhumaev <alexey221284@mail.ru>
2021-05-09 09:30:44 +0400

    fix bugg & hard code in routes

--- papagrek/src/components/Api.jsx
+++ papagrek/src/components/Api.jsx
@@ -1,7 +1,6 @@
 import * as axios from "axios";
 
 export default axios.create({
-	//baseURL: "https://my-json-server.typicode.com/alexey221284/papagrek-react",
 	baseURL: "http://127.0.0.1:5000",
 	responseType: "json"
 });
--- papagrek/src/routes/routes.jsx
+++ papagrek/src/routes/routes.jsx
@@ -5,17 +5,12 @@ import Catalog from "../components/Catalog";
 
 
 const AppRouter = () => {
-
 	return (
 		<BrowserRouter>
 			<Route exact path="/" component={Main}/>
 			<Route path="/catalog" component={Catalog}/>
-
-			/*HARD CODE */
 			<Route path="/products/1" render={() => <Catalog id={1}/>}/>
 			<Route path="/products/2" render={() => <Catalog id={2}/>}/>
-			/*END HARD CODE */
-
 		</BrowserRouter>
 	)
 }
